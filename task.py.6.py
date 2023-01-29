bfec04a59d6f019bbe0487789dcd45bbd35fc10d
Mykola Poberezhnyi <119344168+PobMyk001@users.noreply.github.com>
2023-01-29 22:07:00 +0100

    first_homework_6

--- task.py.6.py
+++ task.py.6.py
@@ -12,23 +12,23 @@ while True:
     if command == 'add':
         print('enter the name:')
         name = input("> ")
-        print('enter the phone number')
-        number = input("> ")
-        Phone_Book[name] = number
-        print('subscriber is added')
-    elif command == 'delete':
+        if name in Phone_Book:
+            print('subscriber alreade exists')
+        else:
+            print('enter the phone number')
+    number = input("> ")
+    Phone_Book['name'] = number
+    print('subscriber is added')
+    if command == 'delete':
         print('enter the name:')
         name = input(">  ")
-
-        del Phone_Book[name]
-        print('phone number delete')
+        if input(name) in Phone_Book:
+            del Phone_Book[name]
+            print('phone number delete')
     elif command == 'list':
         print(Phone_Book.keys())
     elif command == 'show':
-        print('enter the name')
+        print('enter the name:')
         name = input('> ')
-        if Phone_Book[name] == 'name':
-
-        print(Phone_Book.get(name))
-    else:
-        print('subscriber not found')
+        if name in Phone_Book:
+            print(Phone_Book.get(name))
