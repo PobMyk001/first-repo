eac6407110380d8e296b834dfb196b6640dad87e
Mykola Poberezhnyi <119344168+PobMyk001@users.noreply.github.com>
2023-03-06 09:40:11 +0100

    first_homework_6

--- task_11py.2.py
+++ task_11py.2.py
@@ -1,14 +1,11 @@
-class MyCustomeExpetion(Exception):
-    def __init__(self, message=" Custom exception is occured"):
+class MyCustomeExcpetion(Exception):
+    def __init__(self, message="Custom exception is occured"):
         self.message = message
         super().__init__(self.message)
 
         def str(self):
-            message_ = f"{self.message}"
-            return message_
+            return  f"{self.message}"
 
-try:
-    raise MyCustomeExpetion("Custom exception is occured")
-except MyCustomeExpetion as e:
-    x = print(e)
-    
+
+
+
--- tusk_11py.3.py
+++ tusk_11py.3.py
@@ -1,14 +1,13 @@
 class CustomContextManager:
     def __enter__(self):
         print("=" * 10)
-        return self
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("=" * 10)
-        return self
+        if exc_val is not None:
+            print(exc_val)
+        else:
+            print("=" *10)
 
-    def exit(self, ex_type, ex_value, traceback):
-        print("=" * 10)
-        if exit() is not None:
-            print(f"an arror occured:{ex_value}")
-            return True
+
+
